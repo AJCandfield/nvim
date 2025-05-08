d52c0bbd410cfebf7de1c9eec53178fcd6669fd4
Alexander James Candfield <alex.candfield@gmail.com>
2025-05-08 16:44:51 +0100

    feat: treesitter setting, not finished

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.ensure_installed = vim.tbl_extend("force", opts.ensure_installed or {}, {
+      "bash",
+      "dockerfile",
+      "go",
+      "gomod",
+      "hcl",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "terraform",
+      "toml",
+      "vim",
+      "vimdoc",
+      "yaml",
+    })
+
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt.foldenable = true
+    vim.opt.foldlevel = 5
+
+    opts.highlight = { enable = true }
+    opts.indent = { enable = true }
+  end,
+}
